b0bddb519262727e65c02dec888c6c80b84481d3
thilo <thilo@DESKTOP-UUJOHRC>
2018-05-06 21:42:52 +0200

    Interface CHatValidationI hinzugefügt

---
+++ Messenger/src/ChatValidationI.java
@@ -0,0 +1,12 @@
+
+public interface ChatValidationI {
+
+	/**
+	 * checks if a chat with the given id exists
+	 * @param id of the chat
+	 * @return true: chat exists
+	 */
+	public boolean chatIfChatExists(int chat_id);
+	
+
+}
