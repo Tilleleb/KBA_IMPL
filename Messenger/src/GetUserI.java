a4f76101ef58a9751641ba165fae5742b1bf158c
thilo <thilo@DESKTOP-UUJOHRC>
2018-05-13 17:53:01 +0200

    Interface GetUserI erstellt

---
+++ Messenger/src/GetUserI.java
@@ -0,0 +1,11 @@
+
+public interface GetUserI { 
+	
+	/**
+	 * returns the user-id that the username belong to
+	 * @param username	username for which the id is to be returned
+	 * @return 			user-id that belongs to the username
+	 */
+	public int getUser(String username);
+
+}
